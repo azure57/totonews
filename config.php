ede5740e568e600526a1224f26426deb9d409d21
azure57 <hadi.lazu1409@gmail.com>
2018-12-11 09:44:03 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 
-$ticket = "xxxxxxxxxx";
-$meid = "xxxxxxxxxx";
-$os="xxxxxxxxxx";
-$sign="xxxxxxxxxx";
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh53cu62BabO4gpaxjY5pfnajzo7McWSzdXVjkIq0zH53ndy60JFqs6Vyla2jra6KZM3XjZSKo7-Ik2uRo6zWlGWLoQ
+";
+$meid = "39056a65cc7d0306";
+$os="android";
+$sign="25cfadcd3463ff851df4d7023d2dc6cfd0a3d43b72b0f388723e5940951217dd";
